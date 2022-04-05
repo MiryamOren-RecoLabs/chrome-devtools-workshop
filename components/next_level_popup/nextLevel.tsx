a94c693616219d7f3bf209eeece50bbd67f3c02d
Shay Nehmad <shayn@recolabs.ai>
2022-04-05 21:12:45 +0300

    Added the nextLevelPopup to the first level.
    Signed-off-by: Shay Nehmad <shayn@recolabs.ai>

--- components/next_level_popup/nextLevel.tsx
+++ components/next_level_popup/nextLevel.tsx
@@ -1,19 +1,35 @@
 import Success from '../../images/success-icon.png'
 import Image from 'next/dist/client/image'
-import styles from './next-level.module.css'
+import styles from './nextlevel.module.css'
+import { useState } from 'react';
+// @ts-ignore
+import Modal from 'react-modal';
+import PasswordForm from '../password_form/passwordForm';
 
 const customLoader = ({ src }: { src: string}) => {
     return src
 }
 
-const NextLevel = () => {
+interface NextLevelProps {
+    currentLevel: number;
+}
+
+const NextLevel = ({currentLevel}: NextLevelProps) => {
+    const [showNextLevelPrompt, setShowNextLevelPrompt] = useState(false);
     return (
-        <button className={styles.nextLevelButton}>
-            <span className={styles.successIcon}>
-                <Image src={Success} height={23} width={23} loader={customLoader}/>
-            </span>
-            <span>Cracked it!</span>
-        </button>
+        <div className={styles.nextLevel}>
+            <button className={styles.nextLevelButton} onClick={() => setShowNextLevelPrompt(true)}>
+                <span className={styles.successIcon}>
+                    <Image src={Success} height={23} width={23} loader={customLoader}/>
+                </span>
+                <span>Cracked it!</span>
+            </button>
+            <Modal className={styles.nextLevelModal} isOpen={showNextLevelPrompt}>
+                <button onClick={() => setShowNextLevelPrompt(false)}>X</button>
+                <h1>{`What's this level's password?`}</h1>
+                <PasswordForm level={currentLevel} shouldMoveToNextLevel={true}/>
+            </Modal>
+        </div>
     )
 }
 
