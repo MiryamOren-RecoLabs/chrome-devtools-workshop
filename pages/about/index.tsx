7249f031285a2b79867ed901e8e2865cae05d073
Mirym Oren <miryamo@recolabs.ai>
2022-04-11 02:47:08 +0300

    Home & about pages

---
+++ pages/about/index.tsx
@@ -0,0 +1,52 @@
+import React, { useLayoutEffect } from "react";
+import Navbar from '../../components/navbar/navbar'
+import styles from './about.module.css'
+import {  useEffect, useState } from 'react'
+
+const About = () : JSX.Element => {
+  const [photoDimensions, setPhotoDimensions] = useState<{height: number, width: number}>({ width:625, height:700 });
+  function getWindowDimensions() {
+    if (typeof window === 'object') {
+      const { innerWidth: width, innerHeight: height } = window;
+      return {
+        width,
+        height
+      };
+    }
+  } 
+  const calcPhotoDimensions = (): {height: number, width: number} => {
+    const windowDimensions = getWindowDimensions();
+      if (((windowDimensions!.width / 2) * 1.12 ) > (windowDimensions!.height - 75)) {
+        return({ height: (windowDimensions!.height - 75), width: ((windowDimensions!.height - 75) * 0.892) })
+      } else {
+        return({ height: ((windowDimensions!.width/2) * 1.12), width: (windowDimensions!.width / 2) })
+      }
+  }
+  useLayoutEffect(() => {
+    setPhotoDimensions(calcPhotoDimensions);
+    function handleResize() {
+      setPhotoDimensions(calcPhotoDimensions);
+    }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  
+  return (
+    <div className={styles.about}>
+      <Navbar currentPage="About" />
+      <div className={styles.main}>
+        <img src="https://i.postimg.cc/RhPZ7jkH/profile.png" 
+          width={photoDimensions.width}
+          height={photoDimensions.height}/> 
+        <div>
+            <h1>{`Hi👋 I'm Miryam Oren`}</h1>
+            <h2> Front end developer, sci-fi and fashion geek 🤖💃</h2>
+            <h2>Feel free to contact me!</h2>
+            <h2><a href="mailto:miryamoren360@gmail.com">✉️ miryamoren360@gmail.com</a></h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import styles from './home.module.css'
 import mainImageBackgroundStyles from '../styles/common.module.css'
 import Navbar from '../components/navbar/navbar'
+import Link from "next/link"
 
 const Home = () : JSX.Element => {
   return (
     <div className={`${styles.home} ${mainImageBackgroundStyles.mainImageBackground}`}>
       <Navbar currentPage="Home" />
+      <div className={styles.main}>
+        <h1>Chrome devtools Workshop</h1>
+        <h2>Play, debug and understand websites</h2>
+        <Link href="/levels/welcome/level0/" ><span className={styles.mainButton}>Go to the game <span className={styles.animatedSpan}></span>{` >`}</span></Link>
+      </div>
     </div>
   )
 }
